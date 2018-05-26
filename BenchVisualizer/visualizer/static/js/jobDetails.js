11cf893fe52ee7aff1df16a1de24f7cd1bdd8b32
Vasileios Tsarnas <devtsar@gmail.com>
2018-05-26 19:45:01 +0100

    The chart now gets the data from the latest build

--- BenchVisualizer/visualizer/static/js/jobDetails.js
+++ BenchVisualizer/visualizer/static/js/jobDetails.js
@@ -1,9 +1,49 @@
 $(document).ready(function() {
 
+    var bench_names = ["startup", "compiler", "compress", "crypto", "derby", "mpegaudio", "scimark", "serial", "sunflow", "xml"];
+
+    var specjvm_bench = []
+
+    for(i = 0; i < bench_names.length; i++){
+        specjvm_bench.push($('#'+bench_names[i]).text());
+    }
+
+    var trace1 = {
+      x: bench_names,
+      y: specjvm_bench,
+      name: 'Latest build',
+      type: 'bar'
+    };
+    var trace2 = {
+      x: bench_names,
+      y: [1, 0.5, 3, 2, 5, 4, 7, 6, 8, 9],
+      name: 'Previous good build',
+      type: 'bar'
+    };
+
+    var data = [trace1, trace2];
+    var layout = {
+      title: 'SPECjvm benchmarks',
+      xaxis: {
+        title: 'SPECjvm benchmarks',
+        titlefont: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+      yaxis: {
+        title: 'Ops/m',
+        titlefont: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+      barmode: 'group'
+    };
+
     TESTER = document.getElementById('tester');
-    Plotly.plot( TESTER, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }], {
-    margin: { t: 0 } } );
+    Plotly.plot( TESTER, data, layout);
 
  });
