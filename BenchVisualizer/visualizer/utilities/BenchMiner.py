cc96e1118dc8b1e64c97b931e2960e0dee7c5026
Foivos Zakkak <foivos.zakkak@manchester.ac.uk>
2018-12-15 01:56:16 +0200

    Don't expect true always at the end

--- BenchVisualizer/visualizer/utilities/BenchMiner.py
+++ BenchVisualizer/visualizer/utilities/BenchMiner.py
@@ -35,13 +35,8 @@ class BenchMiner:
             return "missing"
 
         sub_console = self.console_specjvm[search_result.start():]
-        # "+ true" comes after the interrupt signal
-        search_term = r".*\+ true.*"
+        search_term = r".*\[Pipeline\].*"
         search_result = re.search(search_term, sub_console)
-        '''
-        All the specjvm commands should ALWAYS be followed by a 'true' command.
-        For the sake of coverage, in the non - likely event that 'true is not found' treat the benchmark as missing.
-        '''
         if search_result == None:
             return "missing"
         sub_bench_output = sub_console[0:search_result.end()]
